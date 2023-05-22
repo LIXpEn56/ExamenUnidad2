234d1c6996cbd6f12eb0a68cc421308ef2bf9c80
Sandra Lizette Sanchez Ramirez <129222040+LIXpEn56@users.noreply.github.com>
2023-05-22 14:00:48 -0600

    Add files via upload

---
+++ ExamenUnidad2/src/examenunidad2/ExamenUnidad2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examenunidad2;
+
+/**
+ *
+ * @author 
+ */
+public class ExamenUnidad2 {
+    public void Sandra69971() {
+        String nombre = "Sandra Lizette Sanchez Ramirez";
+        String matricula = "69971";
+        String correo = "al069971";
+        
+        String resultado = "Nombre: " + nombre + "\n" + "Matrciula: " + matricula + "\n" +"Correo electronico: " + correo;
+        
+        System.out.println(resultado);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ExamenUnidad2 dato = new ExamenUnidad2 ();
+        dato.Sandra69971();
+    }
+    
+}
